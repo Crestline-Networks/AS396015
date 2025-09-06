659483e4d8b5923ec18bd98101f3cddd8ab0d399
James Curtin <jameswcurtin@gmail.com>
2025-09-06 00:19:59 -0400

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,30 @@
+const MarkdownIt = require('markdown-it');
+const md = new MarkdownIt({ html: true, breaks: true });
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ 'src/assets': 'assets' });
+  eleventyConfig.addGlobalData('year', new Date().getFullYear());
+
+  eleventyConfig.addFilter('md', s => md.render(s || ''));
+
+  eleventyConfig.addCollection('navPages', (api) => {
+    return api.getAll()
+      .filter((item) =>
+        item.url &&
+        item.url !== '/' &&
+        item.url.split('/').filter(Boolean).length === 1 && // top-level like /peering/
+        !item.data.excludeFromNav
+      )
+      .sort((a, b) =>
+        (a.data.nav_order ?? 999) - (b.data.nav_order ?? 999) ||
+        (a.data.title || a.fileSlug).localeCompare(b.data.title || b.fileSlug)
+      );
+  });
+
+  return {
+    dir: { input: 'src', includes: 'layouts', data: '_data', output: '_site' },
+    templateFormats: ['njk', 'md', 'html'],
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk'
+  };
+};
---
+++ README.md
@@ -0,0 +1,20 @@
+# AS396015
+
+Landing page for AS396015
+
+## Running locally
+
+```bash
+npm ci
+npm run dev
+```
+
+Open http://localhost:8080
+
+## Build
+
+```bash
+npm run build
+```
+
+Output in `_site/`.
