86ece9942443b4f3f05db51bee2aef86312d37d1
James Curtin <jameswcurtin@gmail.com>
2025-09-06 00:46:11 -0400

    Specify path prefix

--- .eleventy.js
+++ .eleventy.js
@@ -23,6 +23,7 @@ module.exports = function (eleventyConfig) {
 
   return {
     dir: { input: 'src', includes: 'layouts', data: '_data', output: '_site' },
+    pathPrefix: process.env.PATH_PREFIX || '/',
     templateFormats: ['njk', 'md', 'html'],
     htmlTemplateEngine: 'njk',
     markdownTemplateEngine: 'njk'
